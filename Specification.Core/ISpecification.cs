3a39e36def9c2ba97221bfa114ae45bc87d3ee2c
Alex Yandolin <yandolin.dev@gmail.com>
2018-04-16 18:10:42 +0300

    Specification abstract created.

---
+++ Specification.Core/ISpecification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Specification.Core
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> ToExpression();
+        ISpecification<T> And(ISpecification<T> specification);
+        ISpecification<T> Or(ISpecification<T> specification);
+        ISpecification<T> Not(ISpecification<T> specification);
+    }
+}
---
+++ Specification.Core/Specification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Specification.Core
+{
+    public abstract class Specification<T> : ISpecification<T>
+    {
+        public abstract Expression<Func<T, bool>> ToExpression();
+
+        public ISpecification<T> And(ISpecification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISpecification<T> Or(ISpecification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISpecification<T> Not(ISpecification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
