ca1e0646472a80a8cde3d786c0a40cfec5b53240
Alex Yandolin <yandolin.dev@gmail.com>
2018-04-18 16:20:51 +0300

    ParameterReplacer created.

---
+++ Specification.Core/ParameterReplacer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Specification.Core
+{
+    internal class ParameterReplacer : ExpressionVisitor
+    {
+        private readonly ParameterExpression _parameter;
+
+        internal ParameterReplacer(ParameterExpression parameter)
+        {
+            this._parameter = parameter ?? throw new ArgumentNullException();
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return base.VisitParameter(this._parameter);
+        }
+    }
+}
